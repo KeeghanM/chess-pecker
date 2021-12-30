109af76b3db9b8c78605e57864b0749838b7c88a
Keeghan McGarry <keeghan_m@live.com>
2021-12-30 23:52:12 +0000

    Login page layout

---
+++ pages/tactics.js
@@ -0,0 +1,15 @@
+import { HeartIcon } from "@heroicons/react/solid"
+import Layout from "../components/Layout"
+
+function tactics() {
+    return (
+        <div>
+            <Layout>
+            Tactics <HeartIcon className="w-5 h-5" />
+
+            </Layout>
+        </div>
+    )
+}
+
+export default tactics
