36998fb0b3388c7a828665a7a66197d01558b282
Keeghan McGarry <keeghan_m@live.com>
2022-01-05 13:23:27 +0000

    About Us Page

--- pages/knights.js
+++ pages/knights.js
@@ -5,7 +5,19 @@ function knights() {
   return (
     <div>
       <Layout name="Knight Vision">
-        Knights <HeartIcon className="w-5 h-5" />
+        <div className="flex flex-col p-4 md:p-6 lg:p-12 space-y-2">
+          <h1 className="text-4xl font-bold text-primary">
+            About ChessTraining.app
+          </h1>
+          <div className="space-y-1 pb-6">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+              excepturi consequuntur aliquam porro amet eveniet accusamus velit
+              impedit esse nostrum fugit sint possimus laudantium
+              necessitatibus, molestias nam dolore vel eaque?
+            </p>
+          </div>
+        </div>
       </Layout>
     </div>
   )
