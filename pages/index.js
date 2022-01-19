e362bfc1d2dfd153565142d3b9d0ae071e02e1c3
Keeghan McGarry <keeghan_m@live.com>
2022-01-19 19:56:42 +0000

    Tactics Page live

--- pages/index.js
+++ pages/index.js
@@ -65,8 +65,8 @@ export default function Home() {
             </div>
           </Link>
         </div>
-        <div className=" border-b-2 md:border-2 border-dark hover:cursor-pointer shadow-lg rounded-2xl bg-slate-400 text-slate-600">
-          <Link href="#">
+        <div className="hover:bg-accent-light border-b-2 md:border-2 border-dark hover:cursor-pointer shadow-lg rounded-2xl bg-light">
+          <Link href="/tactics">
             <div className="p-6 md:p-8 lg:p-10 space-y-2">
               <h1 className="text-4xl font-bold">
                 Tactics Training - <i>Coming Soon</i>
--- pages/tactics.js
+++ pages/tactics.js
@@ -1,13 +1,29 @@
 import Layout from '../components/Layout'
+import { useState, useEffect, useContext } from 'react'
+import { UserContext } from '../lib/context'
+import LoginForm from '../components/LoginForm'
+
 
 const tactics = () => {
+  const { user } = useContext(UserContext)
+
   return (
     <div>
       <Layout name="Tactics">
-        <div className="flex flex-col p-4 md:p-6 lg:p-12 space-y-2 text-lg text-dark">
-          <h1 className="text-4xl font-bold text-primary">Tactics</h1>
-          <div className="space-y-1 pb-6">
-            <p>Train tactics using the WoodPecker method - Coming Soon</p>
+        <div className="flex flex-col lg:flex-row space-y-12 p-4 md:p-6 lg:p-12 text-lg text-dark">
+          <div className="space-y-4 lg:w-1/3">
+            <h1 className="text-4xl font-bold text-primary">
+              Tactics Training
+            </h1>
+            <div>
+              Train tactics using the WoodPecker Method developed by GM's Axel
+              Smith, and Hans Tikkanen.
+            </div>
+          </div>
+          <div className="lg:pl-12">
+            <div className="w-2/3 lg:w-1/3 align-middle m-auto lg:m-0">
+              <LoginForm redirect='/tactics' />
+            </div>
           </div>
         </div>
       </Layout>
