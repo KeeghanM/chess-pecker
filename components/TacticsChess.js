cfd7bfa52b17a80bce7502cad4fd051f0f35e9e6
Keeghan McGarry <keeghan_m@live.com>
2022-01-23 16:13:00 +0000

    Time Tracking & SFX

--- components/TacticsChess.js
+++ components/TacticsChess.js
@@ -3,8 +3,14 @@ import { UserContext } from '../lib/context'
 import Chessboard from './TacticsChessboard'
 import { firestore } from '../lib/firebase'
 import { doc, setDoc } from 'firebase/firestore'
+import useSound from 'use-sound'
 
 export default function TacticsChess(props) {
+  const [playCorrect] = useSound('/sounds/correct.mp3', { volume: 0.25 })
+  const [playIncorrect] = useSound('/sounds/incorrect.mp3', { volume: 0.25 })
+
+  let startTime = Date.now()
+
   const { user } = useContext(UserContext)
   const [currentSet, setcurrentSet] = useState(() => {
     let saved = JSON.parse(localStorage.getItem('tactics-set-list'))
@@ -19,6 +25,7 @@ export default function TacticsChess(props) {
   }, [currentSet])
 
   function changePuzzle(i) {
+    startTime = Date.now()
     setpuzzle(currentSet.set.puzzles[i])
   }
 
@@ -26,12 +33,18 @@ export default function TacticsChess(props) {
     let move = from + to + (promotion || '')
     console.log({ player: move, actual: puzzle.moves[moveIndex] })
     if (move == puzzle.moves[moveIndex]) {
+      playCorrect()
       if (moveIndex != puzzle.moves.length - 1) return true
       currentSet.set.rounds[currentSet.set.rounds.length - 1].correct += 1
+    } else {
+      playIncorrect()
     }
-    // TODO: Update Timer Spent
+
+    let currentTime = Date.now()
+    let dif = (currentTime - startTime) / 1000
 
     currentSet.set.rounds[currentSet.set.rounds.length - 1].completed += 1
+    currentSet.set.rounds[currentSet.set.rounds.length - 1].timeSpent += dif
     changePuzzle(
       currentSet.set.rounds[currentSet.set.rounds.length - 1].completed
     )
