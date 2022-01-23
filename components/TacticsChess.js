141b1e455bbd19cbf1e983eab4c315fe22d6bcfb
Keeghan McGarry <keeghan_m@live.com>
2022-01-23 19:09:48 +0000

    Deployment Fix: Setting a const

--- components/TacticsChess.js
+++ components/TacticsChess.js
@@ -33,7 +33,7 @@ export default function TacticsChess(props) {
   }, [currentSet])
 
   function changePuzzle(i) {
-    startTime = Date.now()
+    setstartTime(Date.now())
     setpuzzle(currentSet.set.puzzles[i])
   }
 
