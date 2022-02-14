49a1eff8d3e5c94cda631cdabc6e329622d4c889
Keeghan McGarry <keeghan_m@live.com>
2022-02-14 19:19:52 +0000

    Set List Settings

--- components/tactics/PuzzleSetListItem.js
+++ components/tactics/PuzzleSetListItem.js
@@ -1,53 +1,35 @@
-import PuzzleSetSettings from './PuzzleSetSettings'
+import PuzzleSetSettings from "./PuzzleSetSettings"
+import { percentOf, secondsToTime } from "../../lib/utils"
 
 export default function SetListItem(props) {
   let set = props.set.set
   return (
-    <div className="p-2 rounded-lg my-2 bg-dark text-light">
-      <div className="flex flex-row justify-between">
-        <p className="text-lg font-bold">
-          {set.setName} - {set.rounds.length}/8
-        </p>
-        <p>
-          <PuzzleSetSettings
-            setId={props.set.id}
-            updateList={props.updateList}
-          />
-        </p>
-      </div>
+    <div className="px-12 py-6 space-y-2 my-2 bg-dark text-light max-w-md">
+      <h3 className="text-2xl font-bold">
+        {set.setName} - {set.rounds.length}/8
+      </h3>
       <p>
-        Finished {set.rounds[set.rounds.length - 1].completed} of {set.setSize}{' '}
+        Finished {set.rounds[set.rounds.length - 1].completed} of {set.setSize}{" "}
         puzzles (
         {percentOf(
           set.rounds[set.rounds.length - 1].correct,
           set.rounds[set.rounds.length - 1].completed
-        )}{' '}
+        )}{" "}
         Accuracy)
       </p>
       <p>
         Round Time: {secondsToTime(set.rounds[set.rounds.length - 1].timeSpent)}
       </p>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row gap-6 pt-6">
         <button
           onClick={props.onSelect}
-          className="py-2 px-4 rounded bg-accent-dark hover:bg-accent-light text-dark font-bold"
+          className="px-4 py-2 bg-primary text-light hover:bg-accent-light hover:text-dark transition duration-200"
         >
           Train Set
         </button>
+
+        <PuzzleSetSettings setId={props.set.id} updateList={props.updateList} />
       </div>
     </div>
   )
-}
-
-function secondsToTime(seconds) {
-  let time = new Date(1000 * seconds).toISOString().substr(11, 8)
-  return time
-}
-
-function percentOf(a, b) {
-  // if (b == 0) return '100%'
-  if (a == 0 || b == 0) return '0%'
-
-  let p = Math.round((a / b) * 100)
-  return p + '%'
 }
--- components/tactics/PuzzleSetSettings.js
+++ components/tactics/PuzzleSetSettings.js
@@ -1,10 +1,11 @@
 import { Dialog } from "@headlessui/react"
 import { useState, useContext } from "react"
 import { UserContext } from "../../lib/context"
-import { CogIcon } from "@heroicons/react/solid"
+import { XCircleIcon } from "@heroicons/react/outline"
 import { firestore } from "../../lib/firebase"
 import { doc, deleteDoc, setDoc } from "firebase/firestore"
 import { saveAs } from "file-saver"
+import { percentOf, secondsToTime } from "../../lib/utils"
 
 export default function PuzzleSetSettings(props) {
   const { user } = useContext(UserContext)
@@ -17,54 +18,78 @@ export default function PuzzleSetSettings(props) {
 
   let roundStats = []
   for (let i = set.set.rounds.length - 1; i >= 0; i--) {
-    roundStats.push(<p key={i}>{set.set.rounds[i].completed}</p>)
+    roundStats.push(
+      <div key={i} className="border-b-4 border-primary py-4">
+        <h4 className="text-lg font-bold">Round #{i + 1}</h4>
+        <p>
+          Completed: {set.set.rounds[i].completed}/{set.set.setSize}
+        </p>
+        <div className="flex gap-2">
+          <p>
+            Correct: {set.set.rounds[i].correct}/{set.set.rounds[i].completed}
+          </p>
+          <p>
+            (
+            {percentOf(
+              set.set.rounds[set.set.rounds.length - 1].correct,
+              set.set.rounds[set.set.rounds.length - 1].completed
+            )}{" "}
+            Accuracy)
+          </p>
+        </div>
+        <p>
+          Round Time:{" "}
+          {secondsToTime(set.set.rounds[set.set.rounds.length - 1].timeSpent)}
+        </p>
+      </div>
+    )
   }
   return (
     <>
       <button
-        className="text-light hover:text-primary"
+        className="px-4 py-2 bg-accent-light text-dark hover:bg-primary hover:text-light transition duration-200"
         onClick={() => setDialogOpen(true)}
       >
-        <CogIcon className="w-5 h-5" />
+        Settings
       </button>
       <Dialog
         open={dialogOpen}
         onClose={() => setDialogOpen(false)}
-        className="fixed z-10 inset-0 overflow-y-auto"
+        className="fixed z-10 inset-0 overflow-y-auto font-serif"
       >
         <div className="flex items-center justify-center min-h-screen">
           <Dialog.Overlay className="fixed inset-0 bg-black opacity-30" />
 
-          <div className="relative bg-white rounded px-12 py-6 space-x-2 max-w-lg mx-auto">
-            <div className="w-full flex flex-row justify-end left-[-5px] top-[5px] absolute">
+          <div className="relative bg-white px-24 py-12 mx-auto">
+            <div className="w-full flex flex-row justify-end left-[-50px] md:left-[-5px] top-[5px] absolute">
               <button
-                className="rounded text-xl font-bold text-dark p-2 hover:bg-accent-light text-center bg-accent-dark"
+                className="text-dark hover:text-primary"
                 onClick={() => {
                   setDialogOpen(false)
                 }}
               >
-                X
+                <XCircleIcon className="w-10 h-10" />
               </button>
             </div>
-            <h3 className="text-lg text-primary font-bold">
+            <h3 className="text-3xl text-primary font-bold">
               {set.set.setName}
             </h3>
-            <div>{roundStats}</div>
+            <div className="max-h-[500px] overflow-y-auto">{roundStats}</div>
             <div className="flex flex-row space-x-2 pt-3">
               <button
-                className="py-2 px-4 rounded text-dark bg-accent-dark hover:bg-primary font-bold"
+                className="px-4 py-2 bg-accent-light text-dark hover:bg-primary hover:text-light transition duration-200"
                 onClick={() => renameSet(set, user, props.updateList)}
               >
                 Rename Set
               </button>
               <button
-                className="py-2 px-4 rounded text-dark bg-accent-dark hover:bg-primary font-bold"
+                className="px-4 py-2 bg-accent-light text-dark hover:bg-primary hover:text-light transition duration-200"
                 onClick={() => exportSet(set)}
               >
                 Export Set
               </button>
               <button
-                className="py-2 px-4 rounded text-dark bg-danger hover:bg-accent-light font-bold"
+                className="px-4 py-2 bg-[red] text-dark hover:bg-primary hover:text-light transition duration-200"
                 onClick={() => deleteSet(set, user, props.updateList)}
               >
                 Delete Set
