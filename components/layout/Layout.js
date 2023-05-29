bcc3616788cd18e498ea2c5e4ab979ebad667db7
Keeghan McGarry <keeghan_m@live.com>
2023-05-29 16:41:08 +0100

    Added google ads

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -17,6 +17,11 @@ const Layout = (props) => (
         href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,700;1,300&display=swap"
         rel="stylesheet"
       />
+      <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7394948634783516"
+        crossorigin="anonymous"
+      ></script>
     </Head>
     <div className="flex flex-col h-screen overflow-x-hidden bg-black">
       <Header noCTA={props.noCTA} name={props.name} />
